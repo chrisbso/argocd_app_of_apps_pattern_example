08b3883357e7552e3c2d454b14b080a9511b94d1
Christoffer Bro Sørensen <christofferbrosoer@outlook.com>
2024-06-28 20:09:28 +0200

    Update _turbo.ps1

--- _turbo.ps1
+++ _turbo.ps1
@@ -4,6 +4,7 @@ minikube start
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install argocd argo/argo-cd -n argocd --create-namespace --namespace argocd --version 7.3.2
 sleep 5
-kubectl apply -f bootstrap.yml
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | %{[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($_))} | Write-Output
+kubectl apply -f bootstrap.yml
+sleep 5
 kubectl port-forward service/argocd-server -n argocd 8080:80
