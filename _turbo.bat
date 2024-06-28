13cd182b9422ecb30898e8d5061b7c42a39808bb
Christoffer Bro Sørensen <christofferbrosoer@outlook.com>
2024-06-28 19:31:31 +0200

    Update _turbo.bat

--- _turbo.bat
+++ _turbo.bat
@@ -1,3 +1,4 @@
+minikube start
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install argocd argo/argo-cd -n argocd --create-namespace --namespace argocd --version 7.3.2
 kubectl apply -f bootstrap.yml
