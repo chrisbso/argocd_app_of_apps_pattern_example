af5e6c4f0200eb74359fe7dac57a5dcc864891c8
Christoffer Bro Sørensen <christofferbrosoer@outlook.com>
2024-06-28 19:33:03 +0200

    Create _turbo.sh

---
+++ _turbo.sh
@@ -0,0 +1,5 @@
+minikube start
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install argocd argo/argo-cd -n argocd --create-namespace --namespace argocd --version 7.3.2
+kubectl apply -f bootstrap.yml
+kubectl port-forward service/argocd-server -n argocd 8080:80
