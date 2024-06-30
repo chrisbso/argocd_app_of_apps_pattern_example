7673cdec7fd9d0c84ee4f087de105c1885336461
Christoffer Bro Sørensen <christofferbrosoer@outlook.com>
2024-06-30 14:42:09 +0200

    Updated automated scripts

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,8 +1,9 @@
 # minikube stop
 # minikube delete
+
 minikube start
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install argocd argo/argo-cd -n argocd --create-namespace --namespace argocd --version 7.3.2 --wait --wait-for-jobs
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | %{[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($_))} | Write-Output
-kubectl apply -f app-of-apps.yml && argocd app wait app-of-apps
+kubectl apply -f app-of-apps.yml; argocd login --core | out-null; kubectl config set-context --current --namespace=argocd | out-null; argocd app wait app-of-apps | out-null; kubectl config set-context --current --namespace=default | out-null 
+Write-Host -NoNewLine "ArgoCD admin password: "; kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | %{[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($_))} | Write-Host
 kubectl port-forward service/argocd-server -n argocd 8080:80
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 # minikube stop
 # minikube delete
+
 minikube start
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install argocd argo/argo-cd -n argocd --create-namespace --namespace argocd --version 7.3.2 --wait --wait-for-jobs
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
-kubectl apply -f app-of-apps.yml && argocd login --core && argocd app wait app-of-apps 2>&1 1>/dev/null
+kubectl apply -f app-of-apps.yml && argocd login --core 2>&1 1>/dev/null && kubectl config set-context --current --namespace=argocd 2>&1 1>/dev/null && argocd app wait app-of-apps 2>&1 1>/dev/null && kubectl config set-context --current --namespace=default 2>&1 1>/dev/null
+echo -n "ArgoCD admin password: " && kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
 kubectl port-forward service/argocd-server -n argocd 8080:80
