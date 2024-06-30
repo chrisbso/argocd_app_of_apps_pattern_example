4121355b49639a108e8665f470854bca5bcc9747
Christoffer Bro Sørensen <christofferbrosoer@outlook.com>
2024-06-30 14:18:56 +0200

    Updated automated scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ minikube start
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install argocd argo/argo-cd -n argocd --create-namespace --namespace argocd --version 7.3.2 --wait --wait-for-jobs
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
-kubectl apply -f app-of-apps.yml && argocd app wait app-of-apps
+kubectl apply -f app-of-apps.yml && argocd login --core && argocd app wait app-of-apps 2>&1 1>/dev/null
 kubectl port-forward service/argocd-server -n argocd 8080:80
